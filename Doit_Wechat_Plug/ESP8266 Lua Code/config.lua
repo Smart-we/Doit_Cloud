fbbd5e28ea3eac58624468b05c5c7e3385bc3f65
Jiachao <zhangjiachao0403@gmail.com>
2016-02-26 08:42:42 +0800

    改为自己的设备ID。

--- Doit_Wechat_Plug/ESP8266 Lua Code/config.lua
+++ Doit_Wechat_Plug/ESP8266 Lua Code/config.lua
@@ -1,6 +1,6 @@
 cfg={
-id='DOIT-SN-12672-89599',
-key='2f9ac0ab249f78a211839ac4b53955d8',	 
+id='DOIT-SN-1318C-39727',
+key='87619f0551ea4d2b5f74bbda491f925d',	 
 domain='s.doit.am',
 port=8810,
 apssid='Doit_ESP_'..node.chipid(),
